6d9ec95b6f12184cf395ce49f88f2c379677b327
mohammed <mohammed@gmail.com>
2020-10-20 15:38:30 +0300

    updating mobile responsive

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,7 +3,7 @@ import './recognition.css';
 
 const ImageLinkForm = ({ onButtonSubmit, onInputChange }) => {
     return (
-        <div >
+        <div className="tc">
             <p className='f3 ma2'>This shit Magic Brain will detect faces in your pictures. give it a try !</p>
             <div className='pa3 ma3 shadow-3 br3 w-70 center '>
                 <input onChange={onInputChange} className='w-60 br3 input' type='text' placeholder='Put your image url here !'></input>
--- src/components/Rank/rank.js
+++ src/components/Rank/rank.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import './rank.css';
 const Rank = ({ name, entries }) => {
     return (
-        <div style={{ color: '#bbb' }}>
+        <div className="tc" style={{ color: '#bbb' }}>
             <p className='f3' >{`${name}, your current rank is :`}</p>
             <p className='f2'>#{entries}</p>
         </div>
