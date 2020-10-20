e1d6cd8d5218a7f9b18ea92510cb0d994f8c7100
mohammed <mohammed@gmail.com>
2020-10-20 12:07:55 +0300

    make it better

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -45,7 +45,7 @@ class Signin extends Component {
                 <main className="pa4 black-80 ">
                     <div className="measure ">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                            <legend className="f1 fw6 ph0 mh0">Sign in</legend>
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f5" htmlFor="email-address">Email</label>
                                 <input onChange={this.onSigninEmail} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
