24d5e5bd631081852b1515cce2236d95a0253f74
mohammed <mohammed@gmail.com>
2020-10-20 15:53:36 +0300

    adding favicon

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,7 +3,7 @@ import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, box }) => {
     return (
-        <div className="ma center ">
+        <div className="ma center tc ">
             <div className='absolute ma2 '>
                 <img id="image" width='450px' height='auto' alt='' src={imageUrl}></img>
                 <div className="bounding-box" style={{ top: box.topRow, left: box.leftCol, bottom: box.bottomRow, right: box.rightCol }}></div>
